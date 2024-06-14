9259b4a2ab0fbdde930f7242c4b61d6d43b48f3d
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-13 22:39:54 -0400

    animating sky color

--- app/src/main/java/com/yash/android/bnr/sunset/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/sunset/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.yash.android.bnr.sunset
 
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.animation.AccelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import com.yash.android.bnr.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,6 +36,12 @@ class MainActivity : AppCompatActivity() {
         val heightAnimator = ObjectAnimator
             .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
             .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
         heightAnimator.start()
+        sunsetSkyAnimator.start()
     }
 }
