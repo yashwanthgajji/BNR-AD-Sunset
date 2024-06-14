4d2b332001d9ae1a87890881383464da332ed561
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-13 22:30:40 -0400

    sunset animation

--- app/src/main/java/com/yash/android/bnr/sunset/MainActivity.kt
+++ app/src/main/java/com/yash/android/bnr/sunset/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.yash.android.bnr.sunset
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.yash.android.bnr.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.scene.setOnClickListener {
+            startAnimation()
+        }
+    }
+    private fun startAnimation() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = binding.sky.height.toFloat()
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.start()
     }
 }
